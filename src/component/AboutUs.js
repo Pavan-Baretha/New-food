3e8c394f8c54fba4298dbe1afc550b210f60b3f2
Pavan-Baretha <pawanbaretha7@gmail.com>
2022-01-26 14:10:30 +0530

    test message

--- src/component/AboutUs.js
+++ src/component/AboutUs.js
@@ -5,8 +5,10 @@ function AboutUs() {
       <div className='container'>
            <div className='row'>
                  <div className='col-sm-6'>
+                   Hiiiiiiiii
                  </div>
                  <div className='col-sm-6'>
+                   HIIIIIIIIIIIII
                  </div>
            </div>
       </div>
